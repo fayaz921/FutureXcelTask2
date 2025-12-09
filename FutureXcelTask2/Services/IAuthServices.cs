157c149b475e7323dc65e3389de2c7d727e23516
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-09 12:59:42 +0500

    Update IAuthServices.cs

--- FutureXcelTask2/Services/IAuthServices.cs
+++ FutureXcelTask2/Services/IAuthServices.cs
@@ -1,6 +1,11 @@
-﻿namespace FutureXcelTask2.Services
+﻿using FutureXcelTask2.Models;
+
+namespace FutureXcelTask2.Services
 {
     public interface IAuthServices
     {
+        Task<User?> Signup(SignupRequest request);
+        Task<string?> Login(LoginRequest request);
+        Task<User?> GetUserByEmail(string email);
     }
 }
