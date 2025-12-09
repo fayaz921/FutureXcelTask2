f7f76a3f19c50c89f606fa20e034b5168f445ef4
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-09 12:59:38 +0500

    Update SignupRequest.cs

--- FutureXcelTask2/Models/SignupRequest.cs
+++ FutureXcelTask2/Models/SignupRequest.cs
@@ -1,6 +1,18 @@
-﻿namespace FutureXcelTask2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FutureXcelTask2.Models
 {
-    public class SignUpRequest
+    public class SignupRequest
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
     }
 }
