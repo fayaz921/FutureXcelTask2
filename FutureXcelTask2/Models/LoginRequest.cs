0a7619bf1295f4371169ec17b9db01944548deb3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-09 12:59:28 +0500

    Update LoginRequest.cs

--- FutureXcelTask2/Models/LoginRequest.cs
+++ FutureXcelTask2/Models/LoginRequest.cs
@@ -1,6 +1,14 @@
-﻿namespace FutureXcelTask2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FutureXcelTask2.Models
 {
     public class LoginRequest
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
