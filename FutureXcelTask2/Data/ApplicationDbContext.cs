419b7e56522c19c189fb7fac688f2de64dfd7398
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-10 14:59:23 +0500

    Update ApplicationDbContext.cs

--- FutureXcelTask2/Data/ApplicationDbContext.cs
+++ FutureXcelTask2/Data/ApplicationDbContext.cs
@@ -3,10 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FutureXcelTask2.Data
 {
-    public class ApplicationDbContext  : DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-       : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
 
         public DbSet<User> Users { get; set; }
     }
